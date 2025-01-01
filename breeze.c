144c779f30d2e31edf34f8a8dc53749f8aff39ad
naybyal <officiallynabiel@gmail.com>
2025-01-01 17:24:45 +0530

    disabled echoing

--- breeze.c
+++ breeze.c
@@ -1,6 +1,38 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <termios.h>
+
+//  to turn off echoing...
+void enableRawMode() {
+    struct termios raw;
+
+    tcgetattr(STDERR_FILENO, &raw);
+
+    raw.c_lflag &= ~(ECHO);
+
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+    /*
+        Terminal attributes can be read into a termios struct by tcgetattr(). 
+        After modifying them, you can then apply them to the terminal using tcsetattr(). 
+        The TCSAFLUSH argument specifies when to apply the change: in this case, it waits for all pending output to be written to the terminal, and also discards any input that hasn’t been read.
+
+        The c_lflag field is for “local flags”. A comment in macOS’s <termios.h> describes it as a “dumping ground for other state”. 
+        So perhaps it should be thought of as “miscellaneous flags”. 
+        The other flag fields are c_iflag (input flags), c_oflag (output flags), and c_cflag (control flags), all of which we will have to modify to enable raw mode.
+
+        ECHO is a bitflag, defined as 00000000000000000000000000001000 in binary. 
+        We use the bitwise-NOT operator (~) on this value to get 11111111111111111111111111110111. 
+        We then bitwise-AND this value with the flags field, which forces the fourth bit in the flags field to become 0, and causes every other bit to retain its current value. 
+        Flipping bits like this is common in C.
+    */
+
+}
 
 int main() {
-    printf("Hello! Breeze is a Text Editor written in C\n");
+    printf("Hello! Breeze is a Text Editor written in C\nPress 'q' and Enter to quit!\n\n");
+
+    enableRawMode();
+    char c;
+    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
     return 0;
 }
