878ffdc7e10df1282130fa2d6819f1994bdc4482
naybyal <officiallynabiel@gmail.com>
2025-01-03 14:12:35 +0530

    i dont know jackshit about this

--- breeze.c
+++ breeze.c
@@ -4,6 +4,7 @@
 #include <ctype.h>
 #include <unistd.h>
 #include <errno.h>
+#include <string.h>
 #include <termios.h>
 #include <sys/ioctl.h>
 
@@ -67,16 +68,74 @@ char editorReadKey() {
     return c;
 }
 
+int getCursorPosition(int *rows, int *cols) {
+    char buf[32];
+    unsigned int i = 0;
+
+    if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4)   
+        return 1;
+
+
+    // while (read(STDIN_FILENO, &c, 1) == 1) {
+    //     if (iscntrl(c)) {
+    //         printf("%d\r\n", c);
+    //     } else {
+    //         printf("%d ('%c')\r\n", c, c);
+    //     }
+    // }
+    while (i < sizeof(buf) - 1) {
+        if (read(STDIN_FILENO, &buf[i], 1) != 1)
+            break;
+        if (buf[i] == 'R')
+            break;
+        i++;
+    }
+
+    buf[i] = '\0';
+
+    if (buf[0] != '\x1b' || buf[1] != '[') return -1;
+    if (sscanf(&buf[2], "%d;%d", rows, cols) != 2) return -1;
+    return 0;
+}
+
 int getWindowSize(int *rows, int *cols) {
     struct winsize ws;
 
     if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
-        return -1;
+        if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) {
+            return -1;
+        }
+        editorReadKey();
+        return getCursorPosition(rows, cols);
     } else {
         *cols = ws.ws_col;
         *rows = ws.ws_row;
         return 0;
     }
+}
+// APPEND BUFFER
+struct abuf {
+  char *b;
+  int len;
+};
+
+#define ABUF_INIT {NULL, 0}
+
+void abAppend(struct abuf *ab, const char *s, int len) {
+    char *new = realloc(ab->b, ab->len + len);
+    
+    if (new == NULL) 
+        return;
+    
+    memcpy(&new[ab->len], s, len);
+    
+    ab->b = new;
+    ab->len += len;
+}
+
+
+void abFree(struct abuf *ab) {
+    free(ab->b);
 }
 
 //  INPUT
@@ -93,20 +152,50 @@ void editorProcessKeypress() {
 }
 
 //  OUTPUT
-void editorDrawRows() {
-    for (int y = 0; y < E.screenrows; y++) {
-        write(STDOUT_FILENO, "~\r\n", 3);
+void editorDrawRows(struct abuf *ab) {
+    int y;
+    for (y = 0; y < E.screenrows; y++) {
+        if (y == E.screenrows / 3) {
+            char welcome[80];
+
+            int welcomelen = snprintf(welcome, sizeof(welcome),
+                "Breeze editor -- version %s", BREEZE_VERSION);
+            
+            if (welcomelen > E.screencols) 
+                welcomelen = E.screencols;
+            abAppend(ab, welcome, welcomelen);
+        } else {
+            abAppend(ab, "~", 1);
+        }
+        
+        abAppend(ab, "\x1b[K", 3);
+        if (y < E.screenrows - 1) {
+            abAppend(ab, "\r\n", 2);
+        }
     }
 }
 
 
 void editorRefreshScreen() {
-    write(STDOUT_FILENO, "\x1b[2J", 4);
-    write(STDOUT_FILENO, "\x1b[H", 3);
+    // write(STDOUT_FILENO, "\x1b[2J", 4);
+    // write(STDOUT_FILENO, "\x1b[H", 3);
 
-    editorDrawRows();
+    // editorDrawRows();
 
-    write(STDOUT_FILENO, "\x1b[H", 3);
+    // write(STDOUT_FILENO, "\x1b[H", 3);
+    struct abuf ab = ABUF_INIT;
+    
+    abAppend(&ab, "\x1b[?25l", 6);
+    // abAppend(&ab, "\x1b[2J", 4);
+    abAppend(&ab, "\x1b[H", 3);
+
+    editorDrawRows(&ab);
+    
+    abAppend(&ab, "\x1b[H", 3);
+    abAppend(&ab, "\x1b[?25h", 6);
+
+    write(STDOUT_FILENO, ab.b, ab.len);
+    abFree(&ab);
 }
 
 //  INIT
