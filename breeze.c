47aead911e7c45cff4afa3a5af3aeea25dc1cd96
naybyal <officiallynabiel@gmail.com>
2025-01-03 21:59:24 +0530

    moving to the ends of the line

--- breeze.c
+++ breeze.c
@@ -7,10 +7,13 @@
 #include <string.h>
 #include <termios.h>
 #include <sys/ioctl.h>
-
+#include <sys/types.h>
 // DEFINES
 #define BREEZE_VERSION "0.0.1"
 #define CTRL_KEY(k) ((k) & 0x1f)
+#define _DEFAULT_SOURCE
+#define _BSD_SOURCE
+#define _GNU_SOURCE
 
 enum editorKey {
     ARROW_LEFT = 1000,
@@ -25,11 +28,19 @@ enum editorKey {
 };
 
 // DATA
+typedef struct erow {
+    int size;
+    char *chars;
+} erow;
 
 struct editorConfig {
     int cx, cy;
+    int rowoff;
+    int coloff;
     int screenrows;
     int screencols;
+    int numrows;
+    erow *row;
     struct termios orig_termios;
 } E;
 
@@ -162,9 +173,38 @@ int getWindowSize(int *rows, int *cols) {
         return 0;
     }
 }
+//  ROW OPERATIONS
+void editorAppendRow(char *s, size_t len) {
+    E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
+    int at = E.numrows;
+    E.row[at].size = len;
+    E.row[at].chars = malloc(len + 1);
+    memcpy(E.row[at].chars, s, len);
+    E.row[at].chars[len] = '\0';
+    E.numrows++;
+}
+//  FILE I/O
+void editorOpen(char *filename) {
+    FILE *fp = fopen(filename, "r");
+    if (!fp)
+        die("fopen");
+    
+    char *line = NULL;
+    size_t linecap = 0;
+    ssize_t linelen;
+    linelen = getline(&line, &linecap, fp);
 
+    while ((linelen = getline(&line, &linecap, fp)) != -1) {
+        while (linelen > 0 && (line[linelen - 1] == '\n' || line[linelen - 1] == '\r'))
+            linelen--;    
+        editorAppendRow(line, linelen);
+    }
 
-// APPEND BUFFER
+    free(line);
+    fclose(fp);
+}
+
+//  APPEND BUFFER
 struct abuf {
   char *b;
   int len;
@@ -191,24 +231,40 @@ void abFree(struct abuf *ab) {
 
 //  INPUT
 void editorMoveCursor(int key) {
+    erow *row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+
     switch (key) {
         case ARROW_LEFT:
             if (E.cx != 0) 
                 E.cx--;
+            else if (E.cy > 0) {
+                E.cy--;
+                E.cx = E.row[E.cy].size;
+            }
             break;
         case ARROW_RIGHT:
-            if (E.cx != E.screencols - 1)
+            if (row && E.cx < row->size)
                 E.cx++;
+            else if (row && E.cx == row->size) {
+                E.cy++;
+                E.cx = 0;
+            }
             break;
         case ARROW_UP:
             if (E.cy != 0)
                 E.cy--;
             break;
         case ARROW_DOWN:
-            if (E.cy != E.screenrows - 1)
+            if (E.cy < E.numrows)
                 E.cy++;
             break;
     }
+
+    row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+    int rowlen = row ? row->size : 0;
+
+    if (E.cx > rowlen)
+        E.cx = rowlen;
 }
 
 void editorProcessKeypress() {
@@ -242,27 +298,55 @@ void editorProcessKeypress() {
 }
 
 //  OUTPUT
+void editorScroll() {
+    if (E.cy < E.rowoff) {
+        E.rowoff = E.cy;
+    }
+
+    if (E.cy >= E.rowoff + E.screenrows) {
+        E.rowoff = E.cy - E.screenrows + 1;
+    }
+
+    if (E.cx < E.coloff) {
+        E.coloff = E.cx;
+    }
+
+    if (E.cx >= E.coloff + E.screencols) {
+        E.coloff = E.cx - E.screencols + 1;
+    }
+}
+
 void editorDrawRows(struct abuf *ab) {
     int y;
     for (y = 0; y < E.screenrows; y++) {
-        if (y == E.screenrows / 3) {
-            char welcome[80];
+        int filerow = y + E.rowoff;
+        if (filerow >= E.numrows) {
+            if (E.numrows == 0 && y == E.screenrows / 3) {
+                char welcome[80];
 
-            int welcomelen = snprintf(welcome, sizeof(welcome),
-                "Breeze editor -- version %s", BREEZE_VERSION);
-            
-            if (welcomelen > E.screencols) 
-                welcomelen = E.screencols;
-            int padding = (E.screencols - welcomelen) / 2;
-            if (padding) {
+                int welcomelen = snprintf(welcome, sizeof(welcome),
+                    "Breeze editor -- version %s", BREEZE_VERSION);
+                
+                if (welcomelen > E.screencols) 
+                    welcomelen = E.screencols;
+                int padding = (E.screencols - welcomelen) / 2;
+                if (padding) {
+                    abAppend(ab, "~", 1);
+                    padding--;
+                }
+                while (padding--) 
+                    abAppend(ab, " ", 1);
+                abAppend(ab, welcome, welcomelen);
+            } else {
                 abAppend(ab, "~", 1);
-                padding--;
             }
-            while (padding--) 
-                abAppend(ab, " ", 1);
-            abAppend(ab, welcome, welcomelen);
         } else {
-            abAppend(ab, "~", 1);
+            int len = E.row[filerow].size - E.coloff;
+            if (len < 0) 
+                len = 0;
+            if (len > E.screencols)
+                len = E.screencols;
+            abAppend(ab, &E.row[filerow].chars[E.coloff], len);
         }
         
         abAppend(ab, "\x1b[K", 3);
@@ -274,6 +358,7 @@ void editorDrawRows(struct abuf *ab) {
 
 
 void editorRefreshScreen() {
+    editorScroll();
     struct abuf ab = ABUF_INIT;
     
     abAppend(&ab, "\x1b[?25l", 6);
@@ -282,7 +367,7 @@ void editorRefreshScreen() {
     editorDrawRows(&ab);
 
     char buf[32];
-    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy+1, E.cx+1);
+    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, (E.cx - E.coloff) + 1);
     abAppend(&ab, "\x1b[?25h", 6);
     
     abAppend(&ab, "\x1b[H", 3);
@@ -296,13 +381,21 @@ void editorRefreshScreen() {
 void initEditor() {
     E.cx = 0;
     E.cy = 0;
+    E.numrows = 0;
+    E.row = NULL;
+    E.rowoff = 0;
+    E.coloff = 0;
     if (getWindowSize(&E.screenrows, &E.screencols) == -1) 
         die("getWindowSize");
 }
-int main() {
+int main(int argc, char *argv[]) {
     // printf("Hello! Breeze is a Text Editor written in C\nPress 'q' to quit!\n\n");
+    
     enableRawMode();
     initEditor();
+    if (argc >= 2)
+        editorOpen(argv[1]);
+    
     while (1) {
         editorRefreshScreen();
         editorProcessKeypress();
